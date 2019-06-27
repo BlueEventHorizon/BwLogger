08f35bbbb4a90d5450e77aa25ee428ed9ef8c6aa
k2terada <moonsocean@gmail.com>
2019-06-27 23:29:20 +0900

    Initial Commit

---
+++ EasyLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyLogger
+//
+//  Created by Katsuhiko Terada on 2019/06/27.
+//  Copyright © 2019 Katsuhiko Terada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
