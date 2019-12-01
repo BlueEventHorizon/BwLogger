e55c29af46d5176d9f2933bc85b3cb217b842e08
k2moons <moonsocean@gmail.com>
2019-12-01 21:36:17 +0900

    Add LoggerDependencyFiler

--- Logger/LoggerDependencyFiler.swift
+++ Logger/LoggerDependencyFiler.swift
@@ -8,28 +8,12 @@
 
 import Foundation
 
-class MyLoggerDependency: LoggerDependency {
+public class LoggerDependencyFiler: LoggerDependency {
+    private static let filer = Filer()
+    public init() {}
     public func log(_ formedMessage: String, original: String, level: Logger.Level) {
-        print(formedMessage)
-    }
-    public func preFix(_ level: Logger.Level) -> String {
-        switch level {
-        case .enter:    return "❤️❤️❤️❤️"
-        case .exit:     return "♠️♠️♠️♠️"
-
-        case .error:    return "[🔥ERROR]"
-        case .fatal:    return "[🔥FATAL]"
-        default: return DefaultLoggerDependency().preFix(level)
-        }
-    }
-    public func isEnabledClassAndMethodName(_ level: Logger.Level) -> Bool {
-        return false
-    }
-    // true: Add file name and line number at the end of log
-    public func isEnabledFileAndLineNumber(_ level: Logger.Level) -> Bool {
-        switch level {
-        case .info:     return false
-        default:        return true
-        }
+        LoggerDependencyFiler.filer.open()
+        LoggerDependencyFiler.filer.write(formedMessage)
+        LoggerDependencyFiler.filer.close()
     }
 }
