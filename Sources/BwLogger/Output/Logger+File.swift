53d3e66fe3563d281d7fd510052da3cd99f2e91a
k2moons <moonsocean@gmail.com>
2021-07-09 23:45:58 +0900

    add file

---
+++ Sources/BwLogger/Output/Logger+File.swift
@@ -0,0 +1,37 @@
+//
+//  FileLogger.swift
+//  BwTools
+//
+//  Created by k_terada on 2020/07/16.
+//  Copyright © 2020 k2moons. All rights reserved.
+//
+
+import BwLogger
+import Foundation
+
+public protocol FileLoggerDependency {
+     func open()
+     func close()
+     func write(_ text: String)
+}
+
+public final class FileLogger: LogOutput {
+
+    let dependency: FileLoggerDependency
+
+    public init(dependency: FileLoggerDependency) {
+        self.dependency = dependency
+    }
+
+    public func log(_ context: LogInformation) {
+        let separator: String = context.message.isEmpty ? "" : " --"
+        
+        let message = context.level == .info ?
+            "\(context.prefix)\(context.addSpacer(" ", to: context.message))\(separator) \(context.methodName)" :
+            "\(context.prefix) [\(context.timestamp())] [\(context.threadName)]\(context.addSpacer(" ", to: context.message))\(separator) \(context.methodName) \(context.fileName):\(context.line))"
+
+        dependency.open()
+        dependency.write(message)
+        dependency.close()
+    }
+}
